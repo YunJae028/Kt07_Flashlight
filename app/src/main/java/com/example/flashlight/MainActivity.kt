320507e166785b83f0e1fe0529afa021f677ca07
facvis <dy_5088@naver.com>
2020-09-19 20:53:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.flashlight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.intentFor
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val torch = Torch(this)
+        /* 액티비티에서 플래시 켜는 구조
+        flashSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked){
+                torch.flashOn()
+            }else{
+                torch.flashOff()
+            }
+        }
+
+         */
+
+        flashSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked){
+                startService(intentFor<TorchService>().setAction("on"))
+            }else{
+                startService(intentFor<TorchService>().setAction("off"))
+            }
+        }
+    }
+}
